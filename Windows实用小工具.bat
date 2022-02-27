ad07c0303b713e59a2561a07c859a079d6060e5d
2096779623 <57583560+2096779623@users.noreply.github.com>
2022-02-27 11:47:54 +0800

    新增功能：去除桌面右下角水印（win11）
    system requirements not met

--- Windows实用小工具.bat
+++ Windows实用小工具.bat
@@ -287,7 +287,7 @@ echo 109.打开DHCP                              129.管理员取得所有权             
 echo 110.设置静态IP                            130.获取所有杀毒软件名字以及路径              150.office启用所有宏                  170.自定义系统属性信息
 echo 111.刷新DNS缓存                           131.删除windows资源管理器上方的百度网盘       151.修改计算机名                      171.指定程序以管理员身份运行
 echo 112.文件/文件夹选项                       132.删除windows资源管理器上方的酷狗音乐       152.打开/关闭剪贴板历史记录(Win10)    172.修复网络图标变成英文(win10)
-echo 113.修改按下电源按钮时"无操作"            133.打开/关闭临时IPV6地址                     153.修改此电脑上方六个文件夹的位置
+echo 113.修改按下电源按钮时"无操作"            133.打开/关闭临时IPV6地址                     153.修改此电脑上方六个文件夹的位置    173.去除桌面右下角水印(win11)
 echo 114.添加按住Ctrl,再按两次ScrollLock键蓝屏 134.预防autorun病毒(U盘)                      154.启用/禁用SuperFetch/Sysmain(Win10)
 echo 115.修复开始菜单无法打开                  135.修复在文件夹选项中无法显示隐藏文件        155.虚拟机与hyper-V共存
 echo 116.强制更新组策略                        136.修复win10家庭版没有组策略                 156.打开/关闭自动修复(win10)
@@ -370,6 +370,7 @@ if %user_input% equ 169 echo 不能恢复！继续吗？ && pause && bcdedit /set %GUID% 
 if %user_input% equ 170 goto diysysinfo
 if %user_input% equ 171 goto runasadmin1
 if %user_input% equ 172 goto fixnetlang
+if %user_input% equ 173 reg add "HKEY_CURRENT_USER\Control Panel\UnsupportedHardwareNotificationCache" /v SV2 /t REG_DWORD /d 0 /f
 if %user_input% equ about goto about
 if %user_input% equ cleartool goto clean
 if %user_input% equ back goto memu
